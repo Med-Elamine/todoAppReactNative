b3a71c2843c260aa8e31335372d6bd1d9850cd74
ELKHAYYAT MOHAMMAD ELAMINE <96497970+Med-Elamine@users.noreply.github.com>
2022-04-17 15:17:24 +0000

    some Update

--- src/components/item/index.js
+++ src/components/item/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity} from "react-native";
+import { Text, TouchableOpacity, View} from "react-native";
 import {MaterialIcons} from '@expo/vector-icons';
 
 import Styles from "./styles";
@@ -7,13 +7,14 @@ import Styles from "./styles";
 const Item = (props) => {
 
     return (
-        <TouchableOpacity 
-            style={Styles.touch} 
-            onPress={() => props.pressHandler(props.item.key)}
-        >
-            <MaterialIcons size={18} color={'#6a5'} style={Styles.icon} name="delete" />
+        <View style={Styles.touch}>
+            <TouchableOpacity 
+                onPress={() => props.pressHandler(props.item.key)}
+            >
+                <MaterialIcons size={18} color={'#6a5'} style={Styles.icon} name="delete" />
+            </TouchableOpacity>
             <Text style={Styles.item}>{props.item.text}</Text>
-        </TouchableOpacity>
+        </View>
     );
 }
 
